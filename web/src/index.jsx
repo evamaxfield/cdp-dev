77eb0aee19bbfa831f646ff3861cd3ed312c46c0
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-02-20 21:47:59 -0800

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-cdp-dev-ocvyjncw",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "cdp-dev",
+        timeZone: "America/Los_Angeles",
+        footerLinksSections: [],
+    },
+    features: {
+        // enableClipping: true,
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
